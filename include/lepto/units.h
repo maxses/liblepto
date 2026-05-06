a1918c5d09deeffb63a59da70a9a64cffe6dffc1
Maximilian Seesslen <dev@seesslen.net>
2026-05-06 20:05:39 +0200

    Units: Made macro values unsigned

--- include/lepto/units.h
+++ include/lepto/units.h
@@ -15,23 +15,23 @@
 /*--- Defines --------------------------------------------------------------*/
 
 
-#define SEC_PER_MIN     60l
-#define MIN_PER_HOUR    60l
-#define MIN_PER_DAY     ( MIN_PER_HOUR * 24l )
-#define SEC_PER_HOUR    3600l
-#define SEC_PER_DAY     ( SEC_PER_HOUR * 24l)
+#define SEC_PER_MIN     60ul
+#define MIN_PER_HOUR    60ul
+#define MIN_PER_DAY     ( MIN_PER_HOUR * 24ul )
+#define SEC_PER_HOUR    3600ul
+#define SEC_PER_DAY     ( SEC_PER_HOUR * 24ul)
 // SEC_PER_YEAR: Without leap years of course, just for approach
-#define SEC_PER_YEAR    ( SEC_PER_DAY  * 365l)
+#define SEC_PER_YEAR    ( SEC_PER_DAY  * 365ul)
 #define MSEC_PER_HOUR   ( SEC_PER_HOUR * MSEC_PER_SEC )
-#define NSEC_PER_SEC    1000000000l     // nano
-#define USEC_PER_SEC    1000000l        // micro
+#define NSEC_PER_SEC    1000000000ul     // nano
+#define USEC_PER_SEC    1000000ul        // micro
 #define USEC_PER_MIN    ( USEC_PER_SEC * SEC_PER_MIN )
-#define MSEC_PER_SEC    ( 1000l )           // milli
+#define MSEC_PER_SEC    ( 1000ul )           // milli
 #define MSEC_PER_MIN    ( MSEC_PER_SEC * SEC_PER_MIN )
-#define USEC_PER_MSEC   ( 1000l )
-#define SUBSEC_PER_SEC  0x100
-#define MSEC_PER_SUBSEC (1000/0x100)
-#define HOUR_PER_DAY    ( 24l )
+#define USEC_PER_MSEC   ( 1000ul )
+#define SUBSEC_PER_SEC  0x100ul
+#define MSEC_PER_SUBSEC (1000ul/0x100ul)
+#define HOUR_PER_DAY    ( 24ul )
 
 #define BYTES_PER_MB    ( 1024 * 1024 )
 

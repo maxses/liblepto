b7e4766adb6b7936ef1c89191167e1a91c6f20b6
Maximilian Seesslen <dev@seesslen.net>
2026-05-14 15:09:31 +0200

    Added config preset

---
+++ include/lepto/presets/config_full.h
@@ -0,0 +1,23 @@
+#ifndef LEPTO_PRESET_CONFIG_FULL_H
+#define LEPTO_PRESET_CONFIG_FULL_H
+//----------------------------------------------------------------------------
+///
+/// \brief  Config for host device
+///
+///         -
+//
+/// \date   20250202
+/// \author Maximilian Seesslen <mes@seesslen.net>
+///
+//----------------------------------------------------------------------------
+
+
+#define CONFIG_LEPTO_LOG_PRETTY_PRINT              1
+#define CONFIG_LEPTO_LOG_ANSI_COLORS               1
+#define CONFIG_LEPTO_SIGNAL_CHAIN                  1
+#define CONFIG_LEPTO_SIGNAL_FUNCTION               1
+#define CONFIG_LEPTO_SIGNAL_METHOD                 1
+
+
+//---fin-----------------------------------------------------------------------
+#endif // ? ! LEPTO_PRESET_CONFIG_FULL_H
